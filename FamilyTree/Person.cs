a8170b4435b2b17059ed45550b94ab44f315e96b
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-12-27 22:19:44 +0200

    Добавьте файлы проекта.

---
+++ FamilyTree/Person.cs
@@ -0,0 +1,121 @@
+﻿using System.Runtime.CompilerServices;
+using FamilyTree;
+
+namespace FamilyTree
+{
+    enum GenderEnum
+    {
+        Мужской,
+        Женский
+    }
+    internal class Person
+    {
+
+
+        public string Name { get; }
+        public GenderEnum Gender { get; }
+        public Person? Mother { get; set; }
+        public Person? Father { get; set; }
+        public Person? Spouse { get; set; }
+
+        private LinkedList<Person> Children = new();
+
+
+
+        public LinkedList<Person> GetChildren()
+        {
+            LinkedList<Person> listChildren = new(Children);
+            return listChildren;
+        }
+
+        public void SetChildren(Person children)
+        {
+            this.Children.AddLast(children);
+            if (this.Gender == GenderEnum.Мужской)
+            {
+                children.Father = this;
+            }
+            else
+            {
+                children.Mother = this;
+            }
+        }
+
+
+        //Родители неизвестны
+        public Person(string name, GenderEnum gender)
+        {
+            this.Name = name;
+            this.Gender = gender;
+
+        }
+        //Родители известны
+        public Person(string name, GenderEnum gender, Person father, Person mother)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Father = father;
+            this.Mother = mother;
+            father.Children.AddLast(this);
+            mother.Children.AddLast(this);
+
+        }
+
+        //Родители известны + супруг/супруга
+        public Person(string name, GenderEnum gender, Person father, Person mother, Person spouse)
+        {
+            this.Name = name;
+            this.Gender = gender;
+            this.Father = father;
+            this.Mother = mother;
+            this.Spouse = spouse;
+            father.Children.AddLast(this);
+            mother.Children.AddLast(this);
+        }
+
+        public override string? ToString()
+        {
+            string result = $"{Name}";
+            return result;
+        }
+
+        public static void GetTreeConsole(Person person, int depth = 0)
+        {
+            if (depth == 0)
+            {
+                if (person.Spouse != null)
+                {
+                    Console.WriteLine($"({person.ToString()}:{person.Spouse.ToString()})");
+                    depth++;
+                }
+                else
+                {
+                    Console.WriteLine($"{person.ToString()}");
+                    depth++;
+                }
+
+
+            }
+            if (person == null || person.Children.Count == 0)
+            {
+                Console.WriteLine(String.Concat(Enumerable.Repeat("-----", depth)));
+            }
+            else
+            {
+                foreach (var item in person.Children)
+                {
+                    if (item.Spouse != null)
+                    {
+                        Console.WriteLine($"{String.Concat(Enumerable.Repeat("----", depth))} ({item.ToString()}:{item.Spouse.ToString()})");
+                        GetTreeConsole(item, depth + 1);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{String.Concat(Enumerable.Repeat("----", depth))} {item.ToString()}");
+                        GetTreeConsole(item, depth + 1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FamilyTree/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace FamilyTree
+{
+    internal class Program
+    {
+        //Доработайте приложение генеалогического дерева таким образом чтобы программа выводила на экран близких
+        //родственников(жену/мужа). Продумайте способ более красивого вывода с использованием горизонтальных и вертикальных черточек.
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Иван", GenderEnum.Мужской);
+            Person p2 = new Person("Маша", GenderEnum.Женский);
+            p1.Spouse = p2;
+            p2.Spouse = p1;
+            Person p3 = new Person("Петр", GenderEnum.Мужской, p1, p2);
+            Person p11 = new Person("Света", GenderEnum.Женский);
+            p3.Spouse = p11;
+            p11.Spouse = p3;
+            Person p4 = new Person("Анна", GenderEnum.Женский, p1, p2);
+
+            Person p10 = new Person("Андрей", GenderEnum.Мужской);
+            p4.Spouse = p10;
+            p10.Spouse = p4;
+
+            Person p5 = new Person("Сергей", GenderEnum.Мужской, p10, p4);
+
+            Person p6 = new Person("Олег", GenderEnum.Мужской, p10, p4);
+            Person p14 = new Person("Евгения", GenderEnum.Женский);
+            p6.Spouse = p14;
+            p14.Spouse = p6;
+            Person p8 = new Person("Ольга", GenderEnum.Женский, p3, p11);
+            Person p13 = new Person("Егор", GenderEnum.Мужской);
+            p13.Spouse = p8;
+            p8.Spouse = p13;
+
+            Person p7 = new Person("Александра", GenderEnum.Женский, p3, p11);
+
+
+
+            Person p9 = new Person("Никита", GenderEnum.Мужской, p6, p14);
+            Person p12 = new Person("Олеся", GenderEnum.Женский, p13, p8);
+
+
+
+            Person.GetTreeConsole(p1);
+
+
+
+        }
+    }
+}
